c645f15e7201bac011140c750cbea2b8f5a6ab5b
Andrey Vasquez <william20026@gmail.com>
2021-12-06 13:47:50 -0500

    Modal Details

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Logo from "../../logo.png";
+
 import './Modal.css'
 
 function Modal({ children, onClose }) {
     return <div className='modal'>
         <div className='modal-content'>
             <button className='btn' onClick={onClose}>🅧</button>
+            <figure className="App-logo">
+                <img alt="Giffy logo" src={Logo} />
+            </figure>
             {children}
         </div>
     </div>
